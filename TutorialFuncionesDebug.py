938cf50efafe2cd9a2dba4811d890051b91cddc4
Oscar Diaz <oscar.diaz.romero.al@iespoligonosur.org>
2024-09-24 12:41:29 +0200

    Correccion de un fallo

--- TutorialFuncionesDebug.py
+++ TutorialFuncionesDebug.py
@@ -2,9 +2,6 @@
 #Hacer el ejemplo con una un programa que tenga 4 funciones : sumar, restar, multiplicar y dividir.
 #Explicar el panel dónde se ven las variable locales y globales.
 
-a = int(input("Introduce el primer numero: "))
-b = int(input("Introduce el segundo numero: "))
-
 def sumar(a,b):
     suma = a+b
     return suma
